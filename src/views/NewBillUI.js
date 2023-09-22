ca04827e1b9cc2122277b8f3a05c7ff6b953da83
Belaid Solene <solene.belaid@greta-cfa-aquitaine.academy>
2023-09-22 10:08:13 +0200

    error message clearer

--- src/views/NewBillUI.js
+++ src/views/NewBillUI.js
@@ -56,7 +56,7 @@ export default () => {
                   <div class="col-half">
                     <label for="file" class="bold-label">Justificatif</label>
                     <input required type="file" class="form-control blue-border" data-testid="file" accept="image/jpeg, image/png" />
-                    <span id="errorFormatFile">Veuillez sélectionner un fichier JPEG ou PNG valide.</span>
+                    <span id="errorFormatFile">Veuillez sélectionner un fichier au format JPEG, JPG ou PNG valide.</span>
                 </div>
             </div>
             <div class="row">
